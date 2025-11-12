217934ad44835a0f149d4d4c1fa8a23da569bd65
salierismuse <Isaacformato@gmail.com>
2025-11-12 01:12:05 -0500

    small cpu stuff

---
+++ src/cpu.rs
@@ -0,0 +1,17 @@
+pub struct Cpu {
+    pub v: [u8; 16],
+    pub i: [u8; 16],
+    pub st: [u8; 12],
+    pub memory: [u8l 4096,
+    pub input u16,
+
+    pub pc: u16,
+    pub sp: u8,
+
+    pub delay_timer u8,
+    pub sound_timer u8,
+}
+
+impl Cpu {
+
+}
